9d74c982cd11f4eee400f612cd918097563f4cef
MuhammadAdibasyah <mhmmd.dedeb@gmail.com>
2022-04-24 15:40:59 +0700

    edit file and jobs

--- src/test/java/starter/book/Get.java
+++ src/test/java/starter/book/Get.java
@@ -11,7 +11,6 @@ import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
 public class Get {
 
     protected static String url = "https://demoqa.com/BookStore/";
-    protected static String url2 = "https://demoqa.com/BookStore/";
 
 
     @Step("I set GET api endpoints")
@@ -37,28 +36,4 @@ public class Get {
         Assert.assertEquals(isbn,"9781449325862");
         Assert.assertEquals(title,"Git Pocket Guide");
     }
-
-//    @Step("I set GET Book api endpoints")
-//    public String setApiEndpoints(){
-//        return url2 + "v1/Book?ISBN=tess";
-//
-//    }
-//    @Step("I send GET Book HTTP request")
-//    public void sendRequest(){
-//        SerenityRest.given().get(setGetApiEndpoints());
-//    }
-//
-//    @Step("I receive valid HTTP response code 400")
-//    public void receiveValidCode400(){
-//        restAssuredThat(response -> response.statusCode(400));
-//    }
-//
-//    @Step("I receive error message")
-//    public void receiveErrorMessage(){
-//        Response response = SerenityRest.lastResponse();
-//        int code = response.getBody().jsonPath().get("code");
-//        String message = response.getBody().jsonPath().get("message");
-//        Assert.assertEquals(code,"1205");
-//        Assert.assertEquals(message,"ISBN supplied is not available in Books Collection!");
-//    }
 }
